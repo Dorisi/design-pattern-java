c19c4a878caff2f3b2e8891298cbf46f359c80f8
邓小雪 <dggu@qq.com>
2018-09-23 22:44:33 +0800

    防止反射的安全问题

--- src/Singleton/SafeAbout.java
+++ src/Singleton/SafeAbout.java
@@ -1,5 +1,6 @@
 package Singleton;
 
+import java.io.ObjectStreamException;
 import java.io.Serializable;
 
 /**
@@ -7,4 +8,27 @@ import java.io.Serializable;
  * 就加了一个方法，防止反射的
  */
 public class SafeAbout implements Serializable {
+	private static class SingletonClassInstance{
+        private static final SafeAbout safeAbout=new SafeAbout();
+    }
+	// 方法没有同步，调用效率高
+    public static SafeAbout getInstance(){
+        return SingletonClassInstance.safeAbout;
+    }
+    
+    // 防止反射获取多个对象的漏洞
+    // 这种抛异常的方法解决了反射入侵的问题
+    private SafeAbout() {
+		 if (null != SingletonClassInstance.safeAbout) {
+			 throw new RuntimeException();
+		 }
+	}
+    // 防止反序列化获取多个对象的漏洞。
+    // 无论是实现Serializable接口，或是Externalizable接口，当从I/O流中读取对象时，readResolve()方法都会被调用到。
+    // 实际上就是用readResolve()中返回的对象直接替换在反序列化过程中创建的对象。
+    // 当你把对象存储到txt文件中，对象转换成字节码存储，当你在获取对象的数据的时候，字节码转换为该对象，但是这个对象已经不是
+    // 之前的那个对象，所以用 readResolve可以替换成和之前一样的对象
+    private Object readResolve() throws ObjectStreamException {
+        return SingletonClassInstance.safeAbout;
+    }
 }
