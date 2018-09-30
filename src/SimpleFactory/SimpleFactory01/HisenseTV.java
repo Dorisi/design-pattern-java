e4687ae81ea499e56377291a4686b31211236d67
邓小雪 <dggu@qq.com>
2018-09-30 21:16:11 +0800

    简单工厂模式实例一

---
+++ src/SimpleFactory/SimpleFactory01/HisenseTV.java
@@ -0,0 +1,10 @@
+package SimpleFactory.SimpleFactory01;
+
+public class HisenseTV implements TV {
+
+	@Override
+	public void play() {
+		System.out.println("海信电视机播放中......");
+	}
+
+}
---
+++ src/SimpleFactory/SimpleFactory01/TV.java
@@ -0,0 +1,9 @@
+package SimpleFactory.SimpleFactory01;
+/**
+ * 电视接口
+ * @author Cherry
+ *
+ */
+public interface TV {
+	void play();
+}
